45fdb622bd9ce451f99528462ed84ca894afb246
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-08 09:55:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function stringChop(str, size) {
   // your code here
+	let ans = [];
+	let index= 0;
+	for (let i = 0; i < str.length/size+1; i++) {
+		let n = size;
+		let chip = "";
+		while (n>0) {
+			if (str.charAt(index)!==undefined) {
+				chip+=str.charAt(index);
+				index++;
+			}
+			n--;
+		}
+		ans.push(chip)
+	}
+	return [ans];
 }
 
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
+
